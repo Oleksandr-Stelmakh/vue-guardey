48ed87852805aa6b1cfdcaf055edf714c666f4d5
Alexander <astelmah060@gmail.com>
2026-02-18 15:43:41 +0200

    fix: update routing to redirect to step1 and use correct component import

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import PageAir1 from '../pages/PageAir1.vue'
+import step1 from '../pages/registration/step1.vue'
 import PageAir5 from '../pages/PageAir5.vue'
 import PageAir12 from '../pages/PageAir12.vue'
 import PageAir16 from '../pages/PageAir16.vue'
 
 const routes = [
-   { path: '/', redirect: '/PageAir1' },
-   { path: '/PageAir1', component: PageAir1 },
+   { path: '/', redirect: '/step1' },
+   { path: '/step1', component: step1 },
    { path: '/PageAir5', component: PageAir5 },
    { path: '/PageAir12', component: PageAir12 },
    { path: '/PageAir16', component: PageAir16 },
